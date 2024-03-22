cee9390ed537d76b27da1d56fcb059703542bbc7
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-03-22 14:28:37 +0300

    feat: read and insert secret

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,44 @@
-#! /usr/bin/env node
-const yargs = require('yargs');
+#!/usr/bin/env node
+import yargs from 'yargs';
+import chalk from 'chalk';
+import boxen from 'boxen';
+import { getSecrets, insertSecrets } from './repository.js';
 
-const options = yargs
-      .help(true)
-      .argv;
+const invalid_usage = chalk.red("Usage: enter valid arguments or run --help");
+
+const usage = boxen('Secret-Wars');
+
+const arg = yargs(process.argv)
+    .option("l", {
+        alias: "list",
+        describe: "List all secret and password stored",
+        type: "boolean",
+        demandOption: false,
+    })
+    .option("r", {
+        alias: "register",
+        describe: "Register given parameters",
+        type: "array",
+        demandOption: false
+    })
+    .option('d', {
+      alias: "delete",
+      describe: "Delete given paramenters",
+      type: "array",
+      demandOption: false
+    })
+    .help(true)
+    .usage(usage)
+    .argv;
+
+if(arg.d != null) {
+  console.log("Coming soon...");
+}
+
+if(arg.l != null && arg.r == null) {
+    getSecrets();
+}
+
+if(arg.r != null && arg.l == null) {
+  insertSecrets(arg.r);
+}
---
+++ bin/repository.js
@@ -0,0 +1,45 @@
+import * as fs from 'fs';
+
+export const getSecrets = () => {
+    fs.readFile("data.json", (err, json) => {
+        if(err) {
+            console.log(err);
+            return; 
+        }
+        if(json == null || json.length === 0) { 
+            console.log("No secrets found");
+            return; 
+        } 
+
+        const data = JSON.parse(json);
+        console.log(data);
+    })
+}
+
+export const insertSecrets = (toInsert) => {
+    fs.readFile("data.json", (err, json) => {
+        if(err) {
+            console.log(err);
+            return; 
+        }
+        let secrets = [];
+        if(json && json.length > 0) {
+            secrets = JSON.parse(json);
+        }
+        
+        const newSecret = {
+            "subject": toInsert[0],
+            "secret": toInsert[1]
+        }
+
+        secrets.push(newSecret);
+        
+        const updatedJson = JSON.stringify(secrets);
+
+        fs.writeFile("data.json", updatedJson, (err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+    });
+}
